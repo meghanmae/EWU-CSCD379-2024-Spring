fdcab5e7581ec2ef218e64a3f9af5669572abfc9
Meghan <Meg.Woodford@IntelliTect.com>
2024-05-16 13:06:28 -0700

    Set up token controller to send a token to the frontend

---
+++ Wordle.Api/Wordle.Api/Dtos/UserCredentialsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Wordle.Api.Dtos
+{
+    public class UserCredentialsDto
+    {
+        public required string Username { get; set; }
+
+        public required string Password { get; set; }
+    }
+}
